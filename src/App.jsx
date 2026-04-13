0391693407b397998384cfb16b3caf771057a12c
GabyYaneva <s22621642@onlineedu.tu-varna.bg>
2026-04-13 16:23:39 +0300

    adding board, snake and apple

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,34 @@ const[isPlaying,setIsPlaying]=useState(false)
   return (
     <div className="min-h-screen flex flex-col items-center justify=center p-4">
       <h1 className="text-5-1 font-bold text-yellow-400 mb-4">🐍Snake🐍</h1>
-      <p>Wellocome in the game</p>
+      <p>Wellocome to the game</p>
+      <p>Use arrows</p>
       <p className="text-red-400">Score:<span className="text-red-400">{score}</span></p>
-    <div className="text-grey-400 text-sm">
-    <p>Head: X:{snake[0].x} Y:{snake[0].y}</p>
-    <p>Apple: X:{apple.x} Y:{apple.y}</p>
-    <p>Game: {isPlaying?'Started':'OnPause'}</p>
+    <div className="re;ative bg-grey-800 border-4 border-yellow-500 shadow-2x1" 
+    style={{
+      width:GRID_SIZE*CELL_SIZE,
+      height:GRID_SIZE*CELL_SIZE
+    }}>
+    {snake.map((segment,index)=>(
+      <div
+      key={index}
+      className="absolute bg-green-500 rounded-sm"
+      style={{
+        width:CELL_SIZE-5,
+        height:CELL_SIZE-2,
+        left:segment.x*CELL_SIZE+1,
+        top:segment.y*CELL_SIZE+1
+      }}></div> 
+    ))}
+    <div className="absolute bg-red-500 rounded-full"
+    style={{
+      width:CELL_SIZE-4,
+      height:CELL_SIZE-4,
+      top: apple.x*CELL_SIZE+2,
+      left:apple.y*CELL_SIZE+2
+    }}>
+
+    </div>
     </div>
     
     </div>
