80b74f14cdd320172be07996ded9dc1b541e6133
GabyYaneva <s22621642@onlineedu.tu-varna.bg>
2026-04-15 11:54:30 +0300

    adding useEffect to move the snake

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,55 @@ const [gameOver,setGameOver]=useState(false)
 
 const[isPlaying,setIsPlaying]=useState(false)
 
+useEffect(()=>{
+  if(!isPlaying||gameOver) return
+  const gameInterval=setInterval(()=>{
+    setSnake(currentSnake=>{
+      const head={
+        x:currentSnake[0].x+direction.x,
+        y:currentSnake[0].y+direction.y
+      }
+      const newSnake=[head,...currentSnake.slice(0,-1)]
+      return newSnake
+    })
+  },START_SPEED)
+
+  return()=>{
+    clearInterval(gameInterval)
+  }
+},[direction,isPlaying,gameOver])
+
+useEffect(()=>{
+  const handleKeyDown=(e)=>{
+    switch(e.key){
+      case "ArrowUp":
+        if(direction.y!==1) setDirection({x:0,y:-1})
+        break
+      case "ArrowDown":
+        if(direction.y!==-1) setDirection({x:0,y:1})
+        break
+      case "ArrowLeft":
+        if(direction.x!==1) setDirection({x:-1,y:0})
+        break
+      case "ArrowRight":
+        if(direction.x!==-1) setDirection({x:1,y:0})
+        break
+    }
+  }
+  window.addEventListener("keydown",handleKeyDown)
+  return()=>{
+    window.removeEventListener("keydown",handleKeyDown)
+  }
+},[direction])
+
   return (
     <div className="min-h-screen flex flex-col items-center justify=center p-4">
       <h1 className="text-5-1 font-bold text-yellow-400 mb-4">🐍Snake🐍</h1>
       <p>Wellocome to the game</p>
       <p>Use arrows</p>
       <p className="text-red-400">Score:<span className="text-red-400">{score}</span></p>
-
+<button className="bg-red-500 rounded border-3 border-yellow-500 m-4 p-4 " >
+     Play   </button>
      <div className="relative bg-grey-700 border-4 border-yellow-500 shadow-2x1" 
          style={{
       width:GRID_SIZE*CELL_SIZE,
