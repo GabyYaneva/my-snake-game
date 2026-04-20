a272813f2687244a589bf8b7679aaac7d28ab60b
GabyYaneva <s22621642@onlineedu.tu-varna.bg>
2026-04-20 20:36:36 +0300

    Board done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState} from "react";
-import{Board} from "./components/Board";
+import Board from "./components/Board"
 
 const GRID_SIZE = 20;
 const CELL_SIZE = 25;
@@ -17,22 +17,10 @@ export default function App() {
             {score}</span>
     </p>
 <Board gridSize={GRID_SIZE} cellSize={CELL_SIZE}>
-    <div  className="absolute bg-red-500 rounded-full"
-          style={{
-            width: CELL_SIZE - 4,
-            height: CELL_SIZE - 4,
-            left: 15 * CELL_SIZE + 2,
-            top: 5 * CELL_SIZE + 2,
-          }}>
-        <p className="absolute inset-0 flex items-center justify-center text-white font-bold">
-            Board works</p>
-    </div>
+   
 </Board>
 
-<button onClick={() =>setScore(s=>s+10)}
- className="mt-6 px-4 bg-green-500 hover:bg-amber-700 text-white rounded-b-lg transition-colors">
-+10 Score
-</button>
+
         </div>
     );
 }
